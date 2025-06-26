ffada9f1516513e6431da02e9b71b619a2dc7de4
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-06-26 10:38:49 +0300

    added modules exercises

--- JavaScriptBasics/modules/exercises/math/mathOps.js
+++ JavaScriptBasics/modules/exercises/math/mathOps.js
@@ -1,2 +1,8 @@
 // Math operations
 
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+export {add, subtract, multiply, divide};

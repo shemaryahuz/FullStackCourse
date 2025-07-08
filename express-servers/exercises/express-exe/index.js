52eecb697b2a963a131c11a796e8d6f088c2dbd8
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-08 13:01:57 +0300

    adding express exe part2

--- express-servers/exercises/express-exe/index.js
+++ express-servers/exercises/express-exe/index.js
@@ -7,12 +7,33 @@ const port = 3005;
 
 app.get("/", (req, res) =>{
     res.send("Welcome to home!");
-})
+});
+
 app.get("/greet", (req, res) =>{
-    res.send({"msg": `Hi from GET endpoint: ${new Date()}`});
-})
+    res.send({msg: `Hi from GET endpoint: ${new Date()}`});
+});
+
+app.get("/greet/:name", (req, res) => {
+    const name = req.params.name;
+    console.log(`Got request with Name: ${name}`);
+    res.json({msg: `Hello ${name}!`});
+});
+
+app.get("/test", async (req, res) => {
+    try {
+        const response = await fetch(`http://localhost:${port}/greet`);
+        const resJson = await response.json();
+        console.log(resJson);
+        if (resJson.msg && typeof resJson.msg === "string")
+            res.json({result:"ok"});
+        else
+            res.json({result:"fail"});
+    } catch (error) {
+        console.error(`Error testing greet response: ${error}`);
+    }
+});
 
 app.listen(port, (error) => {
     if (error) console.error(`Error listening: ${error}`);
     console.log(`Express server is listening on 'http://localhost:${port}'...`);
-})
+});

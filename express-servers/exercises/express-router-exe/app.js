1183e70700dd626c60f47cc5ef94f5b2aa4b0eb4
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-09 12:25:13 +0300

    adding users rout with get and post(add new user)

--- express-servers/exercises/express-router-exe/app.js
+++ express-servers/exercises/express-router-exe/app.js
@@ -1,13 +1,41 @@
 import express from "express";
 import greetRouter from "./routes/greetRouter.js";
 
+// intialize express application
 const app = express();
 
+// middleware for all requests to convert body to a json object
+app.use(express.json());
+
+// use greet router for '/greet' route
 app.use(greetRouter);
 
+let users = [
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bobe" }
+];
+
+app.get("/users", (req, res) => {
+    res.json(users);
+});
+
+app.post("/users", (req, res) => {
+    const newUser = { 
+        id: users.length + 1, 
+        name: req.body.name
+    };
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
+
+// get port from .env
 const PORT = process.env.PORT;
 
+// run the server
 app.listen(PORT, (error) => {
-    if (error) throw new Error(error);
+    if (error){
+        console.error(`Error running the server: ${error}`);
+        return;
+    }
     console.log(`Server is listening at 'http:/localhost:${PORT}'...`);
-})
+});

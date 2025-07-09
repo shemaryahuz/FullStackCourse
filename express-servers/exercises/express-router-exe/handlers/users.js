6f0fa2b569c9be7ecd3265fa1b38765ffee4dbdc
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-09 13:37:54 +0300

    adding get user by id and create new user

--- express-servers/exercises/express-router-exe/handlers/users.js
+++ express-servers/exercises/express-router-exe/handlers/users.js
@@ -4,14 +4,33 @@ const users = [
 ];
 
 export function getUsers(req, res){
+    // response with all users array
     res.json(users);
 }
 
+export function getUser(req, res){
+    // destruct id from request params
+    const { id } = req.params; 
+    for (let user of users){
+        // if user.id is equal to id value (because it's a string)
+        if (user.id == id){
+            // response with the user object
+            res.json(user);
+        }
+    }
+    // if not found, response with message 'id <id> not found' and status code of not found
+    res.status(404).json({ msg: `id ${id} not found`});
+}
+
 export function createUser(req, res){
+    // destruct name from request body
+    const { name } = req.body;
     const newUser = { 
         id: users.length + 1, 
-        name: req.body.name
+        name: name
     };
+    // add the new user to users array
     users.push(newUser);
+    // response with the new user object and status code of created
     res.status(201).json(newUser);
 }
--- express-servers/exercises/express-router-exe/routes/usersRouter.js
+++ express-servers/exercises/express-router-exe/routes/usersRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createUser, getUsers } from "../handlers/users.js";
+import { createUser, getUser, getUsers } from "../handlers/users.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
+
+router.get("/users/:id", getUser);
 
 router.post("/users", createUser);
 

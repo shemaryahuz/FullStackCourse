ced51158df8d844a7c422626ef06fb0919dcd86f
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-09 11:45:56 +0300

    adding express router and handler

--- express-servers/exercises/express-router-exe/app.js
+++ express-servers/exercises/express-router-exe/app.js
@@ -1,14 +1,13 @@
 import express from "express";
+import greetRouter from "./routes/greetRouter.js";
 
 const app = express();
 
-app.get("/greet", (req, res) => {
-    res.json({ msg: "Hello from '/greet'!"});
-});
+app.use(greetRouter);
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, (error) => {
     if (error) throw new Error(error);
-    console.log(`Server is listening at 'http:/localhost:${PORT}`);
+    console.log(`Server is listening at 'http:/localhost:${PORT}'...`);
 })

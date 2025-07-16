3c393f50904ea5cd21de3514ac4b8e7666db1e38
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-16 11:21:11 +0300

    added MongoDB lesson

---
+++ databases/lessons/mongo-db/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (error) => {
+    if (error) console.log(`Error running: ${error}`);
+    console.log(`Server is running at 'http://localhost:${PORT}'...`);
+});
---
+++ databases/lessons/mongo-db/database/db.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.DB_CONNECTION);
+
+const dbName = "sample_mflix";
+let db;
+
+export async function connect() {
+    if (!db){
+        await client.connect();
+        db = client.db(dbName);
+        console.log(`Connected to MongoDB ${dbName}`)
+    }
+    return db;
+}
+
+connect();

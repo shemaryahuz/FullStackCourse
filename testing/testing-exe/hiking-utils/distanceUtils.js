bf18fbf241e9a3db713447038b88887d61108419
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-07-22 16:18:11 +0300

    added basic node tests

--- testing/testing-exe/hiking-utils/distanceUtils.js
+++ testing/testing-exe/hiking-utils/distanceUtils.js
@@ -1,5 +1,10 @@
 
-
-function kmToMeters(km){
+export function kmToMeters(km){
+    if (typeof km !== 'number') {
+        throw new TypeError('Input must be a number');
+    }  
+    if (km < 0) {
+        throw new RangeError('Input must be a non-negative number');
+    }
     return km * 1000;
 }
---
+++ testing/testing-exe/hiking-utils/distanceUtils.test.js
@@ -0,0 +1,30 @@
+import test from "node:test";
+import assert from "node:assert";
+import { kmToMeters } from "./distanceUtils.js";
+
+test("kmToMeters converts kilometers to meters", () => {
+    assert.strictEqual(kmToMeters(1), 1000);
+    assert.strictEqual(kmToMeters(0.5), 500);
+    assert.strictEqual(kmToMeters(0), 0);
+    assert.strictEqual(kmToMeters(2.5), 2500);
+});
+
+test("kmToMeters throws TypeError for non-number input", () => {
+    assert.throws(() => kmToMeters("1"), TypeError);
+    assert.throws(() => kmToMeters(null), TypeError);
+    assert.throws(() => kmToMeters(undefined), TypeError);
+});
+
+test("kmToMeters throws RangeError for negative input", () => {
+    assert.throws(() => kmToMeters(-1), RangeError);
+    assert.throws(() => kmToMeters(-0.5), RangeError);
+});
+
+test("kmToMeters handles large numbers", () => {
+    assert.strictEqual(kmToMeters(1000000), 1000000000);
+    assert.strictEqual(kmToMeters(123456.789), 123456789);
+});
+test("kmToMeters handles floating point numbers", () => {
+    assert.strictEqual(kmToMeters(0.123), 123);
+    assert.strictEqual(kmToMeters(0.999), 999);
+});

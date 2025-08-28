f0d3be58cee91df32b552362a7f9cd52ce25cf7e
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-08-28 17:07:12 +0300

    add: images and styles to profile cards

--- react/react-exe/src/App.tsx
+++ react/react-exe/src/App.tsx
@@ -2,13 +2,32 @@ import "./css/App.css";
 import Header from "./components/Header";
 import ProfileCard from "./components/ProfileCard";
 
-
 function App() {
   return (
-   <div className="app">
-    <Header />
-    <ProfileCard />
-   </div>
+    <div className="app">
+      <Header />
+      <div className="cards-container">
+        <ProfileCard 
+          imageUrl="src/img/student-avatar.jpg"
+          name="David Cohen"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro voluptate dolor aperiam accusamus facere dolores."
+          personType="Student" 
+        />
+        <ProfileCard 
+          imageUrl="src/img/f-avatar.jpg"
+          name="Bobe Son"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro voluptate dolor aperiam accusamus facere dolores."
+          personType="Friend" 
+        />
+        <ProfileCard 
+          imageUrl="src/img/teacher-avatar.jpg"
+          name="John Doe"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro voluptate dolor aperiam accusamus facere dolores."
+          personType="Teacher" 
+        />
+      </div>
+
+    </div>
   );
 }
 
